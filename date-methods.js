8e3bb77224ccd5e40b219b9cc3510eb474a30045
Big-beatz <kalmaerik@gmail.com>
2022-04-09 08:40:47 +0200

    date-methods gemaakt

--- date-methods.js
+++ date-methods.js
@@ -13,7 +13,8 @@ console.log(currentDay);
 // Uren: 16 (als er een vier in de klok zit op het moment van aanroepen)
 // Minuten: 36 (als het 16:36 is op het moment van aanroepen)
 
-
+const currentTime = currentDay.toDateString()
+console.log(currentTime)
 
 /* Opdracht 2 */
 // Format het date-object dat je bij de vorige vraag hebt gemaakt naar een leesbare datum
